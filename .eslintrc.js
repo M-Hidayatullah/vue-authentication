42f51ed889eb6983e72db737b780bf11604d6747
M-Hidayatullah <hidayatullahblk@gmail.com>
2023-11-15 10:59:29 +0800

    push

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "no-mixed-spaces-and-tabs": 0
+    }
+};
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+//import vue router
+import { createRouter, createWebHistory } from 'vue-router'
+
+//define a routes
+const routes = [
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import( /* webpackChunkName: "login" */ '@/views/auth/Login.vue')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import( /* webpackChunkName: "dashboard" */ '@/views/dashboard/Index.vue')
+    }
+]
+
+//create router
+const router = createRouter({
+    history: createWebHistory(),
+    routes  // config routes
+})
+
+export default router
